8b8972c39355ce69f563e1a055463579061523b4
marianonajeny <119488816+marianonajeny@users.noreply.github.com>
2022-11-30 01:58:03 -0800

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,22 @@
+document.writeln ("<CENTER>");
+document.writeln("<h2>DATA MAHASISWA</h2>");
+document.writeln("<h2>POLITEKNIK AMAMAPARE</h2>");
+document.writeln("<HR WIDTH=600 COLOR=BLACK>");
+document.writeln ("</CENTER>");
+
+// function tekan ()
+//     { 
+//     var nimstr = (document.ffrom.nim.value);    
+//     var namastr = (document.ffrom.nama.value);
+//     var jenisstr = (document.ffrom.jenis.value);
+//     var agamastr = (document.ffrom.agama.value);
+//     var statusstr = (document.ffrom.status.value);
+//     var jurusanstr = (document.ffrom.jurusan.value);
+//     var komentar  = (document.ffrom.komentar.value);
+//     document.ffrom.onim.value =nimstr;
+//     document.ffrom.onama.value =namastr;
+//     document.ffrom.ojenis.value =jenisstr;
+//     document.ffrom.oagama.value =agamastr;
+//     document.ffrom.ostatus.value =statusstr;
+//     document.ffrom.ojurusan.value =jurusanstr;
+//     }
